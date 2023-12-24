e37c02bf9649f93790eb4266064b175092bff3ec
sajibarafatsiddiqui <sajibarafatsiddiqui@gmail.com>
2023-12-24 06:03:40 +0600

    api core Product getMapping completed

---
+++ sajib-microservice/api/src/main/java/se/magnus/api/core/product/Product.java
@@ -0,0 +1,40 @@
+public class Product {
+    private final int productId;
+    private final String name;
+    private final int weight;
+    private final String serviceAddress;
+
+    public Product() {
+        
+    }
+
+
+    public Product(int productId, String name, int weight, String serviceAddress) {
+        this.productId = productId;
+        this.name = name;
+        this.weight = weight;
+        this.serviceAddress = serviceAddress;
+    }
+
+    public int getProductId() {
+        return this.productId;
+    }
+
+
+    public String getName() {
+        return this.name;
+    }
+
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+
+    public String getServiceAddress() {
+        return this.serviceAddress;
+    }
+
+
+
+    }
