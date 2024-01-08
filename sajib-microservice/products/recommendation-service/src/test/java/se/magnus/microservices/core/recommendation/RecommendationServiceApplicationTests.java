ad4f3e875e9a32fd1d0c2e190e7cb7e370579f99
sajibarafatsiddiqui <sajibarafatsiddiqui@gmail.com>
2024-01-09 00:00:38 +0600

    completeted dockerization of each api and control them using docker compose

--- sajib-microservice/products/recommendation-service/src/test/java/se/magnus/microservices/core/recommendation/RecommendationServiceApplicationTests.java
+++ sajib-microservice/products/recommendation-service/src/test/java/se/magnus/microservices/core/recommendation/RecommendationServiceApplicationTests.java
@@ -1,13 +1,7 @@
 package se.magnus.microservices.core.recommendation;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RecommendationServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
