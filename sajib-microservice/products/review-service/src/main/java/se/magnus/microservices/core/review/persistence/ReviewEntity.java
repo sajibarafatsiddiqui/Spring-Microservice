82079ef1fa98d1e4e4d969e63297ca2dfdc9fdba
sajibarafatsiddiqui <sajibarafatsiddiqui@gmail.com>
2024-01-16 08:37:08 +0600

    Added repoositories for all the core microservices

--- sajib-microservice/products/review-service/src/main/java/se/magnus/microservices/core/review/persistence/ReviewEntity.java
+++ sajib-microservice/products/review-service/src/main/java/se/magnus/microservices/core/review/persistence/ReviewEntity.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Version;
 
 @Entity
 @Table(name="reviews",indexes= {@Index(unique = true,name="prod-review-id",columnList="productId,reviewId")})
-public class ReviewEntrity {
+public class ReviewEntity {
     @jakarta.persistence.Id 
     @GeneratedValue
     private int Id;
@@ -21,7 +21,7 @@ public class ReviewEntrity {
     private String subject;
     private String content;
 
-    public ReviewEntrity(int productId, int reviewId, String author, String subject, String content) {
+    public ReviewEntity(int productId, int reviewId, String author, String subject, String content) {
         this.productId = productId;
         this.reviewId = reviewId;
         this.author = author;
---
+++ sajib-microservice/products/review-service/src/main/java/se/magnus/microservices/core/review/persistence/ReviewRepository.java
@@ -0,0 +1,10 @@
+package se.magnus.microservices.core.review.persistence;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+public interface ReviewRepository extends CrudRepository<ReviewEntity,String> {
+    @Transactional(readOnly = true)   
+    List<ReviewEntity> findByProductId(int productId);
+}
