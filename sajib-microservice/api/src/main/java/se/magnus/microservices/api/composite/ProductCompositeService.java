7ff3e5002e0f8df404dea3015fded0ef21cf739f
sajibarafatsiddiqui <sajibarafatsiddiqui@gmail.com>
2024-01-31 12:35:20 +0600

    Added create and delete mapping to the product-acomposite api with sqagger api documentation config.

--- sajib-microservice/api/src/main/java/se/magnus/microservices/api/composite/ProductCompositeService.java
+++ sajib-microservice/api/src/main/java/se/magnus/microservices/api/composite/ProductCompositeService.java
@@ -1,9 +1,12 @@
 package se.magnus.microservices.api.composite;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -23,4 +26,26 @@ public interface ProductCompositeService {
     })
     @GetMapping("/product-composite/{productId}")
     ProductComposite getProduct(@PathVariable int productId);
+    
+    @Operation(summary="{api.product-composite.create-composite-product.description}",
+    description = "${api.product-composite.create-composite-product.notes}")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
+        @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
+})
+    @PostMapping(value="/product-composite",consumes="application/json")
+    void createProduct(@RequestBody ProductComposite body);
+
+    @Operation(
+        summary = "${api.product-composite.delete-composite-product.description}",
+        description = "${api.product-composite.delete-composite-product.notes}")
+    @ApiResponses(value = {
+
+        @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
+        @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
+        })
+    @DeleteMapping("/product-composite/{productId}")
+    void deleteProduct(@PathVariable int productId);
+    
+    
 }
